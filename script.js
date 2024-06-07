b3cfa8df8a2a7ab534e5dd2592822f009385adbd
jenusany <jenusany@outlook.com>
2024-06-07 14:09:10 -0400

    login page frist draft done

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ document.getElementById('search-input').addEventListener('input', function() {
         }
     });
 });
+
+let players = document.querySelectorAll('.players');
+
+players.forEach((event) => {
+    event.addEventListener('click', () => {
+        players.forEach((event) => {
+            event.classList = 'players';
+        });
+        event.classList += ' players-selected';
+        localStorage.setItem("players", Number(event.innerText));
+
+        document.getElementById('player-initials').innerHTML = ``;
+
+        for(let i = 1; i <= Number(event.innerText); i++){
+            document.getElementById('player-initials').insertAdjacentHTML('beforeend', `<input type="text" maxlength="2" class="middle" placeholder="Player ${i} initials" required>`)
+        }
+        confirm()
+    })
+})
+
+function confirm(){
+    document.getElementById('confirm').innerHTML = `<button class="confirm-button">CONFIRM</button>`;
+}
